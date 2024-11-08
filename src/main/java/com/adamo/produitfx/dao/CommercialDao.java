f7ed24b2c27383504773776ace58ec927b63793d
Adamo0 <you@example.com>
2024-11-08 22:35:42 +0100

    Added CommercialDao.java

--- src/main/java/com/adamo/produitfx/dao/CommercialDao.java
+++ src/main/java/com/adamo/produitfx/dao/CommercialDao.java
@@ -1,4 +1,11 @@
 package com.adamo.produitfx.dao;
 
-public class CommercialDao {
+
+import com.adamo.produitfx.models.Commercial;
+
+import java.util.Set;
+
+public interface CommercialDao {
+    Set<Commercial> selectAll(); // Pour récupérer tous les commerciaux
+    void delete(String matricule); // Pour supprimer un commercial par matricule
 }
