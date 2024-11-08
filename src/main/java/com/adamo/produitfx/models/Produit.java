3ae18f746e354257b244926df4220b413e2498eb
Adamo0 <you@example.com>
2024-11-08 22:38:09 +0100

    Added Produit.java

--- src/main/java/com/adamo/produitfx/models/Produit.java
+++ src/main/java/com/adamo/produitfx/models/Produit.java
@@ -1,4 +1,45 @@
 package com.adamo.produitfx.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Produit {
+    private int id;
+    private String name;
+    private String description;
+    private double price;
+    private Commercial commercial;
+
+    public Produit(
+            String name,
+            String description,
+            double price,
+            Commercial selectedCommercial
+    ) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.commercial = selectedCommercial;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Produit produit = (Produit) o;
+        return id == produit.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
 }
