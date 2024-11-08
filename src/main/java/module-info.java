d2154b54432281f776c77753d96f7478028ed14d
Adamo0 <you@example.com>
2024-11-08 22:39:03 +0100

    Added file

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,12 @@
 module com.adamo.produitfx {
     requires javafx.controls;
     requires javafx.fxml;
+    requires static lombok;
+    requires java.sql;
 
 
     opens com.adamo.produitfx to javafx.fxml;
     exports com.adamo.produitfx;
+    exports com.adamo.produitfx.controllers;
+    opens com.adamo.produitfx.controllers to javafx.fxml;
 }
