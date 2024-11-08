bbb10342dbd93b244e19be8d697a1f8b2764b7cb
Adamo0 <you@example.com>
2024-11-08 22:36:38 +0100

    Added ProduitDao.java

--- src/main/java/com/adamo/produitfx/dao/ProduitDao.java
+++ src/main/java/com/adamo/produitfx/dao/ProduitDao.java
@@ -1,4 +1,10 @@
 package com.adamo.produitfx.dao;
 
-public class ProduitDao {
+import com.adamo.produitfx.models.Produit;
+
+import java.sql.SQLException;
+
+public interface ProduitDao {
+    int add(Produit p) throws SQLException; // Pour ajouter un produit
+    int update(Produit p); // Pour mettre à jour un produit
 }
